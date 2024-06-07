387b86d446a2d1d82b361be4fa0b9fa9a3bcb773
WillamJAM <williamjhoanartu@gmail.com>
2024-06-07 07:13:03 -0500

    Archivo modificado 07/06/24

--- Calculadora.js
+++ Calculadora.js
@@ -1,2 +1,25 @@
-// alert(`Hola mundo`);
-console.log(`Logica desconectada`);
+function sumar() {
+    let numero1 = parseInt(document.getElementById(`inputNumero1`).value)
+    let numero2 = parseInt(document.getElementById(`inputNumero2`).value)
+    const suma = numero1+ numero2
+    
+    document.getElementById("result").innerHTML= (`${suma}`);
+}
+function restar() {
+    let numero1 = parseInt(document.getElementById(`inputNumero1`).value)
+    let numero2 = parseInt(document.getElementById(`inputNumero2`).value)
+    const suma = numero1 - numero2
+    document.getElementById("result").innerHTML= (`${suma}`);
+}
+function multiplicar() {
+    let numero1 = parseInt(document.getElementById(`inputNumero1`).value)
+    let numero2 = parseInt(document.getElementById(`inputNumero2`).value)
+    const suma = numero1 * numero2
+    document.getElementById("result").innerHTML= (`${suma}`);
+}
+function dividir() {
+    let numero1 = parseInt(document.getElementById(`inputNumero1`).value)
+    let numero2 = parseInt(document.getElementById(`inputNumero2`).value)
+    const suma = numero1 / numero2 
+    document.getElementById("result").innerHTML= (`${suma}`);
+}
