837af981375295922bb0a91afa6c05772e147f79
WillamJAM <williamjhoanartu@gmail.com>
2024-06-07 19:21:07 -0500

    Modified

---
+++ enviar _datos.js
@@ -0,0 +1,11 @@
+
+function enviar() {
+    let input1 = document.getElementById("input1").value
+    let input2 = document.getElementById("input2").value
+    alert(`Hola ${input1} como estas :)`);
+    alert(`Tu edad es: ${input2}`);
+    let anyoNacimiento = 2024 - input2;
+    alert(`Naciste en el año: ${anyoNacimiento}`);
+}
+
+
